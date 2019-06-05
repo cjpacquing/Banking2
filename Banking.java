2b803a7f9328ab0b1c7e03f2851260a6f38bc8c2
Carl Pacquing <cjpacquing@gmail.com>
2019-06-05 16:23:51 -0400

    Banking file added

---
+++ Banking.java
@@ -0,0 +1,16 @@
+package com.Revature.Banking;
+
+public class Banking {
+
+	public static void main(String[] args) {
+		//Login Prompt:
+		
+		//Customer or Employee?
+		
+		//Username and Password
+		
+		//Display Bank Account Information
+
+	}
+
+}
